d8a839b93da6448869839ebaa63f7ec232824fd1
linxi1699z-source <linxi1699z@gmail.com>
2026-03-30 22:45:50 +0800

    fix: resolve github pages 404 error with relative base and HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Guide from './pages/Guide';
 import { ConfigProvider } from 'antd';
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 解决 GitHub Pages 构建后的绝对路径 404 找不到 asset 的问题
   plugins: [react()],
 })
